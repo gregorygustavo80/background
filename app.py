6db653c0017922c6becdba44b45c88c278010c68
Gregory <gregorygustavo@hotmail.com>
2025-04-11 08:25:23 -0300

    update

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def acessar():
     print("Acessando site...")
     requests.get("https://abaixo-assinado-1.onrender.com/")
 
-schedule.every(1).minutes.do(acessar)
+schedule.every(5).minutes.do(acessar)
 
 while True:
     schedule.run_pending()
